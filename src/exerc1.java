4e6f3bf1feda3bebf1ff67911e2937043e2cc176
84Ribeiro <jpribeiro.ti@gmail.com>
2022-10-07 17:39:49 -0300

    Exercicio 6

--- src/exerc1.java
+++
@@ -1,8 +0,0 @@
-public class exerc1 {
-    
-    //Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Ola, Mundo!");
-    }
-}
